9641841510523225f69a25bf039f7c5fca9a81cc
danhhoa <hoathanhdanh1510@gmail.com>
2024-03-29 00:42:45 +0700

    feat: init project

---
+++ app/components/navbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-between px-10 items-center h-[60px] bg-secondary">
+      <div>TEST</div>
+      <div>wallet</div>
+    </div>
+  );
+};
